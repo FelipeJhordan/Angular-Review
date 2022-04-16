d74db8a4f80ebed4cbc1317e0f536c9b095e7512
felipejhordan <felipejordan.alves@gmail.com>
2022-04-16 18:09:36 -0300

    feat: import shared and pages modules in app module ( root module )

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
+//Native
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
+//Modules
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+
+//Components
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
